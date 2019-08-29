5b8c4ae82a3f28997dd2009cb28f78424e84ed39
CheeseBreadIsGood <49957396+CheeseBreadIsGood@users.noreply.github.com>
2019-08-29 19:24:09 -0400

    work

--- CreateEncrypedDataFromStringNKey.ps1
+++ CreateEncrypedDataFromStringNKey.ps1
@@ -26,19 +26,17 @@
   $SeedPassword = read-host "Seed Key Password" -AsSecureString ##This will be a typed in secret so as to not keep it inside the code
   $EncryptThisNow = read-host "Paste Your Secret Here:" -AsSecureString ##Paste in your text secret so as to not keep it inside the code
   
-  $key =  [Text.Encoding]::UTF8.GetBytes($SeedPassword) 
-  $AESkeySize = 32   # You can use 16, 24, or 32 bytes for AES,which is 128,192,or 256 bits respectivly 
-  
+  $key =  [Text.Encoding]::UTF8.GetBytes($SeedPassword) #Set the $SeedPassword into array of bytes
+  $AESkeySize = 32 # How many bytes do you want? You can use 16, 24, or 32 bytes for AES,which is 128,192,or 256 bits respectivly 
+  #region # Make the bytes the perfect size of 16, 24 or 32 depending on what $AESkeySize is
   while ($key.Length -lt $AESkeySize ) { # Keep doubleing the size of the key until it is greater then $aeskeysize
   $key = $key + $key
   }
   if ($key.Length -gt $AESkeySize ) { #cut the larger then needed key down to the AESkeySize
-  $Key = $Key[0..($AESkeySize -1)]
-  }  #$key is now the right size to use
+  $Key = $Key[0..($AESkeySize -1)] #$key is now the right size to use
+  }  
+   #endregion
+
   
-  $key[0].GetType()
-  $key.Length
-  
-  $Encrypted = ConvertFrom-SecureString $EncryptThisNow -Key $Key 
-  
-  Out-File -InputObject $Encrypted -FilePath "C:\temp\EncryptedPassword.txt"
+  $Encrypted = ConvertFrom-SecureString $EncryptThisNow -Key $Key #This is where the magic happens. AES encryptiton using the $Key from the user
+  Out-File -InputObject $Encrypted -FilePath "C:\temp\EncryptedStringAES.txt"
