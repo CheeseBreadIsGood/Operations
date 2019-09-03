bd5586200d073f66a979083daa03fb2be5648d16
CheeseBreadIsGood <49957396+CheeseBreadIsGood@users.noreply.github.com>
2019-09-03 14:08:40 -0400

    document

--- DecryptFileAuthenticate.ps1
+++ DecryptFileAuthenticate.ps1
@@ -1,17 +1,22 @@
   <#--------------------------------------------------------------------
   .SYNOPSIS
-    Used to create an encrypted file of an arbitrary string Encrypted from a string key password (should be complex)
+    Used to decrypt stings in an encrypted CSV file of Three strings using a typed in SeedPassword (should be complex)
+    Then use them to authenticate to Azure as a Service Principle. 
 
   .DESCRIPTION
-    This function is used to create an encrytped string $EncryptThisNow using a secret password $SeedPassword. It will be then saved to a file
+    Used to read a csv file of Three encrypted string. Then decrypted them using a String key password (Should be complex). 
+    Items to decrypt are for authenticating to Azure as a service principal. Items are SPusername, IAM Key(Password), and the Azure Tenant ID 
 
   .PARAMETER Key
-    Mandatory. The $SeedPassword will be taken and turned into bytes for use as a $key to encrypt your $EncryptThisNow string. 
+     Mandatory. The $SeedPasswordKey will be typed in and turned into bytes for use as a $key to encrypt your $EncryptThisNow string. 
+    There is some padding and/or removal of this byte formated string to make it fit the size required for an AES key. 
+    You can use 16, 24, or 32 bytes for AES,which is 128,192,or 256 bits respectivly
   .INPUTS
-    $SeedPassword - A typed password. $EncryptThisNow - is the string that you want to encrypt using $SeedPassword you inputed
+    $SeedPassword - A typed password. This is the same string that you used to encrypt the strings for the file
+    Not it is used to decrypt the strings
 
   .OUTPUTS
-    $Encrypted - Your fully encrypted string that was encrypted with your $SeedPassword key.
+    (Nothing) But this will authenicate to Azure as a secruity prinicple
 
   .NOTES
     Version:        1.0
@@ -20,11 +25,11 @@
     Purpose/Change: Initial function development
 
   .EXAMPLE
-    $SeedPassword = "justApassword"; $EncryptThisNow = "This is the sting that I want to encrypt witht he $SeedPassword"
+    $SeedPassword = "justApassword987&&"; 
   #>
-  $File = ".\EncryptedCredentials.txt"
+  $File = ".\EncryptedCredentials.txt" #The file is stored at same location and will be on Github repository
   
-  $SeedPasswordKey = read-host "Seed Key Password" -AsSecureString ##This will be a typed in secret so as to not keep it inside the code
+  $SeedPasswordKey = read-host "Seed Key Password" -AsSecureString ##This will be a typed in secret so as to not keep it inside the code. Needs to be the same password for when these strings where encrpted with.
 
   $key =  [Text.Encoding]::UTF8.GetBytes($SeedPasswordKey) #Set the $SeedPassword into array of bytes
   $AESkeySize = 32 # How many bytes do you want? You can use 16, 24, or 32 bytes for AES,which is 128,192,or 256 bits respectivly 
@@ -37,12 +42,12 @@
   }  
   #endregion
   
-  $CredentialItems = Import-Csv $File #get all three items EncryptedPassword,SPname & TenetID
-  $EncryptedPass = ConvertTo-SecureString $CredentialItems.EncryptedPass -Key $key #decrypt the password
-  $CredentialItems.SPname = ConvertTo-SecureString $CredentialItems.SPname -Key $key #decrypt the password
-  $CredentialItems.SPname = [System.Net.NetworkCredential]::new("", $CredentialItems.SPname).Password #back to a normal string (not secure)
-  $CredentialItems.TenantID  = ConvertTo-SecureString $CredentialItems.TenantID -Key $key #decrypt the password
-  $CredentialItems.TenantID = [System.Net.NetworkCredential]::new("", $CredentialItems.TenantID).Password #back to a normal string (not secure)
+  $CredentialItems = Import-Csv $File #get all three items EncryptedPassword,SPname & TenetID into a handy object
+  $EncryptedPass = ConvertTo-SecureString $CredentialItems.EncryptedPass -Key $key #decrypt the password. This is the only one that stays as a securestring
+  $CredentialItems.SPname = ConvertTo-SecureString $CredentialItems.SPname -Key $key #decrypt the password into a securestring
+  $CredentialItems.SPname = [System.Net.NetworkCredential]::new("", $CredentialItems.SPname).Password #back to a normal string (not securestring)
+  $CredentialItems.TenantID  = ConvertTo-SecureString $CredentialItems.TenantID -Key $key #decrypt the password into a securestring
+  $CredentialItems.TenantID = [System.Net.NetworkCredential]::new("", $CredentialItems.TenantID).Password #back to a normal string (not securestring)
 
 $credentials = New-Object System.Management.Automation.PSCredential($CredentialItems.SPname,$EncryptedPass) #build cretentials for loggin into Azure
 Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $CredentialItems.TenantID ## log into Azure
