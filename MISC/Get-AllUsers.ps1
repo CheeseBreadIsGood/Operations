8bc5401d5ae3a8646739bd8934264c46ad1c567f
CheeseBread <mryan@Mendelsonconsulting.com>
2020-05-06 16:21:25 -0400

    Fixed an extra sort

--- MISC/Get-AllUsers.ps1
+++ MISC/Get-AllUsers.ps1
@@ -7,4 +7,4 @@ Get-ADUser -Filter {Enabled -eq $true} -SearchBase "OU=CloudUsers,DC=Cloud,DC=lo
 Write-output "----------------------------------------------"
 Write-Output "----------- All Enabled users -------------" 
 Write-output "----------------------------------------------"
-Get-ADUser -Filter {Enabled -eq $true}  -Properties LastLogonDate | select samaccountname, Name | Sort-Object Name, LastLogonDate | Sort-Object LastLogonDate
+Get-ADUser -Filter {Enabled -eq $true}  -Properties LastLogonDate | select samaccountname, Name | Sort-Object LastLogonDate
