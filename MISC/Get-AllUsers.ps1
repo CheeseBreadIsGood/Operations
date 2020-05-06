d5e8f89ad0bef7a4b37b3a806edc3c6f95d21ece
CheeseBread <mryan@Mendelsonconsulting.com>
2020-05-06 16:27:21 -0400

    d

--- MISC/Get-AllUsers.ps1
+++ MISC/Get-AllUsers.ps1
@@ -2,9 +2,9 @@
 Write-output "----------------------------------------------"
 Write-Output "--------- Users in OU=CloudUsers --------------"
 Write-output "----------------------------------------------"
-Get-ADUser -Filter {Enabled -eq $true} -SearchBase "OU=CloudUsers,DC=Cloud,DC=local" -Properties LastLogonDate | select samaccountname, Name, LastLogonDate | Sort-Object LastLogonDate
+Get-ADUser -Filter {Enabled -eq $true} -SearchBase "OU=CloudUsers,DC=Cloud,DC=local" -Properties LastLogonDate | Select-Object samaccountname, Name, LastLogonDate | Sort-Object LastLogonDate
 
 Write-output "----------------------------------------------"
 Write-Output "----------- All Enabled users -------------" 
 Write-output "----------------------------------------------"
-Get-ADUser -Filter {Enabled -eq $true}  -Properties LastLogonDate | select samaccountname, Name | Sort-Object LastLogonDate
+Get-ADUser -Filter {Enabled -eq $true}  -Properties LastLogonDate | Select-Object samaccountname, Name | Sort-Object LastLogonDate
