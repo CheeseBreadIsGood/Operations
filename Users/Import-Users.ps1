7a1954f3b75bb130f29afb0299fbb0723f7e0b50
CheeseBreadIsGood <49957396+CheeseBreadIsGood@users.noreply.github.com>
2021-07-20 14:44:03 -0700

    added email
    added mandatory email for DUO

--- Users/Import-Users.ps1
+++ Users/Import-Users.ps1
@@ -1,12 +1,10 @@
 
-
-
-
 $UserList = Import-Csv -Path C:\NoobehIT\ServerSetup\MISCsoftware\usernames.csv 
 $pass = Read-Host "Enter Password for every user" -AsSecureString
 foreach ($User in $UserList) {
    $User.FirstName = $User.FirstName  -replace '[^a-zA-Z0-9]', '' ##.trim()  ##Clear leading and trailing spaces
    $User.LastName  = $User.LastName -replace '[^a-zA-Z0-9]', '' ##.trim()  ##Clear leading and trailing spaces
+   $User.Email  = $User.Email.trim()                             ##Need this for DUO
 
      $Attributes = @{
 
@@ -24,7 +22,7 @@ foreach ($User in $UserList) {
         DisplayName = "$($User.FirstName) $($User.LastName)"
         UserPrincipalName = $User.FirstName.Substring(0,1) + $User.LastName + "@Cloud.local"
         SamAccountName = $User.FirstName.Substring(0,1) + $User.LastName
-
+        EmailAddress = $User.Email
         GivenName = $User.FirstName
         Surname = $User.LastName
 
@@ -33,5 +31,8 @@ foreach ($User in $UserList) {
      }
 
   New-ADUser @Attributes
+ #$Attributes
+}
+
  #$Attributes
 }
