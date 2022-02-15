0e8e534f559e27363a648fa8da394b455bb9c0ce
Mike Ryan <MRyan@mendelsonconsulting.com>
2022-02-15 13:55:21 -0800

    Added more delays

--- VMconfigure/VMconfigureMain.ps1
+++ VMconfigure/VMconfigureMain.ps1
@@ -352,11 +352,13 @@ Function Set-PostWork{  #-------------------------------------------------------
   #do this after the domain controller is created and rebooted.
 If ( -not (Confirm-Tasklog -LogName PostLog))
    {
+    Start-sleep 120
    Set-GPOsettings
    Start-Sleep 120
    Set-Misc
    Start-sleep 120
    Set-NTFSsecurity
+   Start-sleep 120
    Set-SoftwareInstall
    Unregister-ScheduledTask -TaskName "RunonceAfterReboot" -Confirm:$false #don't let it run again
    Set-TaskLog -LogName PostLog
